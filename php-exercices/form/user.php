96aa7407b4e34d36244cb510270e2f78d0dcd94e
Juliane Blier <blier.juliane@gmail.com>
2017-01-16 14:46:06 +0100

    Récupération des informations du formulaire

--- php-exercices/form/user.php
+++ php-exercices/form/user.php
@@ -5,9 +5,14 @@
 		private $_lastName;
 
 		public function __construct($first, $last){
-			$this->firsName = $first;
+			$this->firstName = $first;
 			$this->lastName = $last;
+		}
+
+		public function displayUser(){
+			echo "Le nom de l'utilisateur est : $this->firstName $this->lastName";
 		}
 	}
 
-
+	$juliane = new User($_GET['first_name'], $_GET['last_name']);
+	$juliane->displayUser();
