4054654e0f427742c2393bb2e958299cbb131a6c
Juliane Blier <blier.juliane@gmail.com>
2017-01-16 14:34:29 +0100

    Création de la class user, attributs et constructeur

---
+++ php-exercices/form/user.php
@@ -0,0 +1,13 @@
+<?php 
+
+	class User{
+		private $_firstName;
+		private $_lastName;
+
+		public function __construct($first, $last){
+			$this->firsName = $first;
+			$this->lastName = $last;
+		}
+	}
+
+
